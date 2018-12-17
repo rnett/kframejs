06ead82c31cb4766600ce82e3201ad2fe804163f
Ryan Nett <rnett@calpoly.edu>
2018-12-16 16:09:42 -0800

    list elements

---
+++ src/main/kotlin/com/rnett/kframejs/dom/ListElements.kt
@@ -0,0 +1,53 @@
+package com.rnett.kframejs.dom
+
+import com.rnett.kframejs.dom.classes.ListElement
+import com.rnett.kframejs.structure.*
+import org.w3c.dom.HTMLLIElement
+import org.w3c.dom.HTMLOListElement
+import org.w3c.dom.HTMLUListElement
+
+@KFrameElementDSL
+inline fun <ParentType> ParentType.ul(
+    klass: String = "",
+    id: String = "",
+    crossinline builder: ElementBuilder<ListElement<HTMLUListElement>> = {}
+)
+        where ParentType : AnyDisplayElement, ParentType : CanHaveElement<*> =
+    ListElement<HTMLUListElement>("ul", this).invoke {
+        if (klass != "")
+            this.klass = klass
+        if (id != "")
+            this.id = id
+        builder()
+    }
+
+
+@KFrameElementDSL
+inline fun <ParentType> ParentType.ol(
+    klass: String = "",
+    id: String = "",
+    crossinline builder: ElementBuilder<ListElement<HTMLOListElement>> = {}
+)
+        where ParentType : AnyDisplayElement, ParentType : CanHaveElement<*> =
+    ListElement<HTMLOListElement>("ul", this).invoke {
+        if (klass != "")
+            this.klass = klass
+        if (id != "")
+            this.id = id
+        builder()
+    }
+
+
+@KFrameElementDSL
+inline fun ListElement<*>.li(
+    klass: String = "",
+    id: String = "",
+    crossinline builder: StandardDisplayElementBuilder<HTMLLIElement> = {}
+) =
+    StandardDisplayElement<HTMLLIElement>("li", this).invoke {
+        if (klass != "")
+            this.klass = klass
+        if (id != "")
+            this.id = id
+        builder()
+    }
--- src/main/kotlin/com/rnett/kframejs/dom/classes/ListElements.kt
+++ src/main/kotlin/com/rnett/kframejs/dom/classes/ListElements.kt
@@ -1,2 +1,8 @@
 package com.rnett.kframejs.dom.classes
 
+import com.rnett.kframejs.structure.CanHaveElement
+import com.rnett.kframejs.structure.Element
+import org.w3c.dom.HTMLElement
+
+class ListElement<U : HTMLElement>(tag: String, parent: CanHaveElement<*>) : Element<ListElement<U>, U>(tag, parent)
+
