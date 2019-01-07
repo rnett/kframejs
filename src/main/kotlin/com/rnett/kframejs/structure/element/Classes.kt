ca52cd50051e47191960d4757de9efdc9745083f
Ryan Nett <rnett@calpoly.edu>
2019-01-06 20:04:05 -0800

    class enum delegate

--- src/main/kotlin/com/rnett/kframejs/structure/element/Classes.kt
+++ src/main/kotlin/com/rnett/kframejs/structure/element/Classes.kt
@@ -1,8 +1,14 @@
 package com.rnett.kframejs.structure.element
 
+import com.rnett.kframejs.structure.addons.HasClass
+import kotlinx.serialization.enumMembers
 import org.w3c.dom.asList
+import kotlin.properties.ReadWriteProperty
+import kotlin.reflect.KClass
+import kotlin.reflect.KProperty
 
 class Classes(val element: AnyElement) : MutableSet<String> {
+
     private val raw get() = this.element.underlying.classList
 
     override fun add(element: String): Boolean {
@@ -47,4 +53,20 @@ class Classes(val element: AnyElement) : MutableSet<String> {
 
     override fun isEmpty(): Boolean = size == 0
 
+    inline fun <reified EC> by(): ClassEnumDelegate<EC> where EC : Enum<EC>, EC : HasClass = by(EC::class)
+    fun <E> by(enumClass: KClass<E>) where E : Enum<E>, E : HasClass = ClassEnumDelegate<E>(this, enumClass)
+}
+
+class ClassEnumDelegate<E>(val classes: Classes, val enumClass: KClass<E>) :
+    ReadWriteProperty<Any, E?> where E : Enum<E>, E : HasClass {
+    private val values = enumClass.enumMembers().associateBy { it.klass }
+
+    override fun getValue(thisRef: Any, property: KProperty<*>): E? =
+        classes.find { it in values }?.let { values[it] }
+
+    override fun setValue(thisRef: Any, property: KProperty<*>, value: E?) {
+        classes.removeAll { it in values }
+        if (value != null)
+            classes.add(value.klass)
+    }
 }
